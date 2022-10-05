d8034f8105772e46da0982459df5643c372f0fcd
Myroslav <myroslavtp@gmail.com>
2022-10-05 19:15:30 +0300

    deleted plugins, added vanilla jquery

--- js/script.js
+++ js/script.js
@@ -1,39 +1,71 @@
-document.addEventListener('DOMContentLoaded', () =>  {
+$(document).ready(function(){
 
-  $(document).ready(function(){
     $(".hover, .icon").hover(function(){
       $(this).css("color", "#FFFFFF");
       }, function(){
       $(this).css("color", "#8D91A3");
     });
 
-    $(".my-rating").starRating({
-      starShape: 'rounded',
-      totalStars: 5,
-      initialRating: 4,
-      strokeColor: '#FFDD3F',
-      activeColor: '#FFDD3F',
-      hoverColor: '#FFDD3F',
-      ratedColor: '#FFDD3F',
-      strokeWidth: 10,
-      starSize: 20,
-      disableAfterRate: true,
+
+    /// STARS
+
+      $(".star-1").click(function() {
+          $(".all-star").css("fill", "#8D91A3")
+          $(".star-1").css("fill", "#FFDD3F");
+
+      });
+      $(".star-2").click(function() {
+          $(".all-star").css("fill", "#8D91A3");
+          $(".star-1, .star-2").css("fill", "#FFDD3F");
+
+      });
+      $(".star-3").click(function() {
+          $(".all-star").css("fill", "#8D91A3")
+          $(".star-1, .star-2, .star-3").css("fill", "#FFDD3F");
+
+      });
+      $(".star-4").click(function() {
+          $(".all-star").css("fill", "#8D91A3");
+          $(".star-1, .star-2, .star-3, .star-4").css("fill", "#FFDD3F");
+
+      });
+      $(".star-5").click(function() {
+          $(".all-star").css("fill", "#8D91A3");
+          $(".star-1, .star-2, .star-3, .star-4, .star-5").css("fill", "#FFDD3F");
+
+      });
+
+
+
+
+
+
+
+
     });
 
-    $('#circle').circleProgress({
-      value: 0.85,
-      size: 75,
-      startAngle: 300,
-      thickness: 4,
-      emptyFill: "#B7BACD",
-      fill: {
-        gradient: ["#00D4BE", "#1AE780"]
-      }
-    });
-
-
-  });
+    
 
 
 
-})
+
+
+
+
+    
+
+      
+
+  
+
+
+
+
+
+
+
+
+    
+
+
+
